3378a75dcf95b18ca34eb636adfe6d94330f748f
hongnguyen <kimhongn89@gmail.com>
2015-06-01 17:59:58 +0700

    add comment line

--- protected/config/main.php
+++ protected/config/main.php
@@ -8,7 +8,7 @@
 return array(
 	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
 	'name'=>'Elisoft Quiz',
-
+	//change something
 	// preloading 'log' component
 	'preload'=>array('log'),
     'timeZone' => 'Asia/Ho_Chi_Minh',
